02d8e05b7b6f58458109d8cb193d72860a8c7eaf
Uttam <gawasuttam@gmail.com>
2020-01-17 18:07:59 +0530

    Commit 1st

---
+++ FirstGitApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FirstGitApp
+//
+//  Created by Uttam Gawas on 1/17/20.
+//  Copyright © 2020 Uttam Gawas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("uttam")
+    }
+
+
+}
+
